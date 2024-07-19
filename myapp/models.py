fc28cd912c1c627981d1286b26ca3d49a0de8ba2
sanjesh <sanjeshrg9@gmail.com>
2024-07-19 14:22:11 +0530

    Initial commit for the Django

---
+++ myapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=200)
+    Business_name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.product_name
